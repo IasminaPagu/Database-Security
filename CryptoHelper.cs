44d71f2c27857709c024ab3fc4e1168fe290a614
Iasmina <paguiasmina0508@gmail.com>
2025-06-02 22:58:49 +0300

    Adaugare noi functionalitati

---
+++ CryptoHelper.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public static class CryptoHelper
+{
+    private static readonly string keyString = "E9#xB1$eW7!qZ5@k";
+    private static readonly string ivString = "L0ckD@tA12345678";
+
+    public static byte[] Encrypt(string plainText)
+    {
+        using Aes aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(keyString);
+        aes.IV = Encoding.UTF8.GetBytes(ivString);
+
+        using MemoryStream ms = new();
+        using CryptoStream cs = new(ms, aes.CreateEncryptor(), CryptoStreamMode.Write);
+        using StreamWriter sw = new(cs);
+        sw.Write(plainText);
+        sw.Close();
+
+        return ms.ToArray();
+    }
+
+    public static string Decrypt(byte[] cipherBytes)
+    {
+        using Aes aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(keyString);
+        aes.IV = Encoding.UTF8.GetBytes(ivString);
+
+        using MemoryStream ms = new(cipherBytes);
+        using CryptoStream cs = new(ms, aes.CreateDecryptor(), CryptoStreamMode.Read);
+        using StreamReader sr = new(cs);
+        return sr.ReadToEnd();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Server=localhost;Database=securedb;User ID=root;Password=root;";
+
+        string name = "Ion Popescu";
+        string cnp = "1980512345678";
+        string card = "1234-5678-9012-3456";
+
+        byte[] encryptedCNP = CryptoHelper.Encrypt(cnp);
+        byte[] encryptedCard = CryptoHelper.Encrypt(card);
+
+        try
+        {
+            using MySqlConnection connection = new(connectionString);
+            connection.Open();
+
+            string insertQuery = "INSERT INTO users (Name, EncryptedCNP, EncryptedCard) VALUES (@name, @cnp, @card)";
+            using MySqlCommand cmd = new(insertQuery, connection);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@cnp", encryptedCNP);
+            cmd.Parameters.AddWithValue("@card", encryptedCard);
+            cmd.ExecuteNonQuery();
+
+            string selectQuery = "SELECT Name, EncryptedCNP, EncryptedCard FROM users";
+            using MySqlCommand selectCmd = new(selectQuery, connection);
+            using MySqlDataReader reader = selectCmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string dbName = reader.GetString("Name");
+                byte[] dbCNP = (byte[])reader["EncryptedCNP"];
+                byte[] dbCard = (byte[])reader["EncryptedCard"];
+
+                string decryptedCNP = CryptoHelper.Decrypt(dbCNP);
+                string decryptedCard = CryptoHelper.Decrypt(dbCard);
+
+                Console.WriteLine($"Nume: {dbName}, CNP: {decryptedCNP}, Card: {decryptedCard}");
+            }
+
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Eroare: " + ex.Message);
+        }
+    }
+}
